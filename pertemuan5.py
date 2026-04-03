d93e31a76fd4b92cd19c4b46a332a18a949e5b93
irwanssy <irwangg20@gmail.com>
2026-04-03 21:37:17 +0700

    ini Set

--- pertemuan5.py
+++ pertemuan5.py
@@ -1,22 +1,43 @@
-#dictionary
-userlogin1 = {  
-    "nama"    : "Irwansyah",
-    "role"    : "Admin",
-    "email"   : "irwanh@gmail.com",
-    "No.telp" : "0882001032640"
-}
+#membuat struktur data dictionary
+userLogin = {"name": "alfin", "age":18, "role":"maksman"}
+print(type(userLogin))
+# Mengakses Data
 
-userlogin2 = {
-    "nama"    : "Naruto",
-    "role"    : "User",
-    "email"   : "jkw@gmail.com",
-    "No.telp" : "0882001032640"
-}
+print(f"Nama Akun : {userLogin['name']}")
+print(f"Umur Akun : {userLogin['age']} Tahun")
+print(f"Role Akun : {userLogin['role']}")
 
-for key,value in userlogin1.items():
-    print(key, userlogin1[key])
+#akses data seluruh
+print(userLogin.items())
+print(userLogin.keys())
+print(userLogin.values())
 
-for key in userlogin1:
-    if key in userlogin2:
-        if userlogin1[key] == userlogin2[key]:
-            print(f"{key} sama")
+# Menambah Data kedalam dictionary big-O O(1)
+userLogin["email"] = "alfinalfaruq99@gmail.com"
+print(userLogin)
+
+# Menghapus Data dari dictionary big-O O(1)
+userLogin.pop("age")
+print(userLogin)
+
+#mengubah data dalam dictionary big-O O(1)
+userLogin["role"] = "maksman"
+print(userLogin)
+
+#nested dictionary
+dbUser = {"user1": {"name": "alfin", "age": 18, "role": "maksman"},
+            "user2": {"name": "amhar", "age": 19, "role": "tank"},
+            "user3": {"name": "abi", "age": 20, "role": "mage"}}
+
+print(dbUser)
+
+#akses value base key
+print(dbUser["user1"])
+
+#melakukan pencarian data dalam dictionary
+finword = input("Masukkan nama user yang ingin dicari: ")
+if finword in dbUser:
+    print("Data ditemukan")
+    print(dbUser[finword])
+else:
+    print("Data tidak ditemukan")
