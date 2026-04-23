b39ad7fd12f3f86b15a58320458a860fc4824f0b
MuchmenAdrians <irwangg20@gmail.com>
2026-04-23 08:32:18 +0700

    hallo tugas

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("Ini Title")
-st.text("Hello my name is Much Mentari Irwansyah and this is my first streamlit app")
+st.text("Hello my name is Much Mentari Irwansyah and this is my first streamlit app.")
 st.button("Click on me")
